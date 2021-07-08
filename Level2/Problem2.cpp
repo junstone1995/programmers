fecf3886bf030956831546fa908a89aa1431c364
Junseok Lee <boris0730@gmail.com>
2021-07-08 22:25:34 +0900

    Level 2

---
+++ Level2/Problem2.cpp
@@ -0,0 +1,18 @@
+include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int n) {
+    string answer = "";
+    string tmp = "412";
+    while(n != 0)
+    {
+        answer = tmp[n%3] + answer;
+        if (n % 3 == 0)
+            n = (n / 3) - 1;
+        else
+            n /= 3;
+    }
+    return answer;
+}
