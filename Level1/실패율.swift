8e33be0669054fa0fe0263fe11cbed1e501fc94a
Junseok Lee <boris0730@gmail.com>
2021-08-20 14:40:08 +0900

    실패율

---
+++ Level1/실패율.swift
@@ -0,0 +1,26 @@
+import Foundation
+
+func solution(_ N:Int, _ stages:[Int]) -> [Int] {
+
+    var value: [Int: Int] = [:]
+    for stage in stages {
+        if !value.isEmpty && value.keys.contains(stage) { value[stage]! += 1; continue }
+        value[stage] = 1
+    }
+
+    var result: [(stage: Int, value: Double)] = []
+    print(value)
+    var success: Int = stages.count
+    print(success)
+    for stage in 1...N {
+        if !value.keys.contains(stage) { result.append((stage, 0)); continue }
+        result.append((stage, Double(value[stage]!) / Double(success)))
+
+        success -= value[stage]!
+        print(success)
+    }
+
+    result.sort { return $0.value == $1.value ? $0.stage < $1.stage : $0.value > $1.value }
+    return result.compactMap { $0.stage }
+
+}
